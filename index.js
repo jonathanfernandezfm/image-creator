76603c423a800ad9bb0fb96b74b19c8e17c155c2
Poak <jonathanfernandezfm@gmail.com>
2020-11-11 21:51:59 +0100

    🔧 Factorizing and new font

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const { createCanvas, loadImage } = require('canvas')
+const { registerFont, createCanvas, loadImage } = require('canvas')
 const { client, client_secret } = require('./config/auth.json')['wow'];
 const wowAPI = require("./lib/wowAPI");
 
@@ -11,6 +11,8 @@ const wowClient = new wowAPI(client, client_secret);
 
 const WIDTH = 1120
 const HEIGHT = 290
+
+registerFont('./assets/FrizQuadrata.ttf', { family: 'FrizQuadrata' })
 
 const canvas = createCanvas(WIDTH, HEIGHT)
 const context = canvas.getContext('2d')
@@ -49,34 +51,39 @@ app.get('/create-image.png', async (req, res) => {
     return;
   }
 
-  loadImage(icon_url).then(image => {
-    context.drawImage(image, 25, 58, 175, 175)
-    
-    loadImage('./assets/achieve-back.png').then(image1 => {
-      context.drawImage(image1, 0, 0, WIDTH, HEIGHT)
-      
-      context.textAlign = 'center'
-      context.textBaseline = 'top'
-      context.fillStyle = '#fff'
-      context.font = '30pt Arial'
-      context.shadowColor="black";
-      context.shadowBlur=2;
-      context.lineWidth=2;
-      context.shadowOffsetX = 3;
-      context.shadowOffsetY = 3;
-      context.fillText(`${text}`, WIDTH/2+30, 120)
+  let image, imageBack;
 
-      context.font = '15pt Arial'
-      context.shadowColor = 'transparent';
-      context.fillStyle = '#222'
-      context.fillText(`${date}`, WIDTH/2+30, 192)
-      
-      const buffer = canvas.toBuffer('image/png');
-      res.contentType('image/png')
-      res.write(buffer);
-      res.end();
-    })
-  })
+  try {
+    image = await loadImage(icon_url);
+    imageBack = await loadImage('./assets/achieve-back.png');
+  } catch (err) {
+    console.log("FAILED TO LOAD IMAGES", err);
+    res.status(500).json({status: 500, error: 'Error loading images', message: 'Error loading images'})
+    return;
+  }
+  
+  context.drawImage(image, 25, 58, 175, 175)
+  context.drawImage(imageBack, 0, 0, WIDTH, HEIGHT)
+  context.textAlign = 'center'
+  context.textBaseline = 'top'
+  context.fillStyle = '#fff'
+  context.font = '30pt FrizQuadrata'
+  context.shadowColor="black";
+  context.shadowBlur=2;
+  context.lineWidth=2;
+  context.shadowOffsetX = 3;
+  context.shadowOffsetY = 3;
+  context.fillText(`${text}`, WIDTH/2+30, 120)
+
+  context.font = '24pt FrizQuadrata'
+  context.shadowColor = 'transparent';
+  context.fillStyle = '#111'
+  context.fillText(`${date}`, WIDTH/2+30, 188)
+  
+  const buffer = canvas.toBuffer('image/png');
+  res.contentType('image/png')
+  res.write(buffer);
+  res.end();
 })
 
 app.listen(process.env.PORT || 3000, () => {
